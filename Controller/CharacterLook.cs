01f5390ffa52b52b6ddb047ddef087f4df63a3ba
Emile <emilebouchard99@gmail.com>
2024-01-06 23:07:59 -0500

    Controller CharacterLook

---
+++ Controller/CharacterLook.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using EasyCharacterMovement;
+using UnityEngine;
+
+namespace Matroska.Controller
+{
+	public class CharacterLook : EasyCharacterMovement.CharacterLook
+	{
+		public void DisableCameraInput()
+		{
+		
+		}
+
+		public void EnableCameraInput()
+		{
+		
+		}
+	}
+
+}
